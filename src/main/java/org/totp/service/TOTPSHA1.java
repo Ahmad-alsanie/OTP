004947622f61fe275497c59caf0d99318ead8b17
Ahmad Alsanie <ahmad.alsanie92@gmail.com>
2018-02-11 09:26:51 +0200

    Util SHAs class added - TOTPSHAs related classes functionality

--- src/main/java/org/totp/service/TOTPSHA1.java
+++ src/main/java/org/totp/service/TOTPSHA1.java
@@ -1,61 +1,15 @@
 package main.java.org.totp.service;
 
-import java.security.GeneralSecurityException;
-import java.time.Instant;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 
 import main.java.org.totp.util.HashType;
-import main.java.org.totp.util.StringConverter;
+import main.java.org.totp.util.SHAs;
 
 public class TOTPSHA1 implements TOTP {
-
+	private static final String EXCEPTION_MSG ="Something went wrong with SHA1";
 	@Override
 	public String generateTOTP(long seconds, int numberOfDigits, String username, char[] password) {
-		Instant instant = Instant.now();
-		long timeStamp = instant.toEpochMilli() / 1000;// per second
-		long initValue = 0;
-		String moves = "0";
-		long timeSteps = (timeStamp - initValue) / seconds;
-		moves = Long.toHexString(timeSteps).toUpperCase();
-
-		String result = null;
-		byte[] hash;
-		// moving factor 16 bytes
-		while (moves.length() < 16)
-			moves = "0" + moves;
-
-		byte[] timeCode = StringConverter.strToByteArray(moves);
-		byte[] userKey = StringConverter.strToByteArray(username + new String(password));
-		hash = sha1(HashType.SHA1.getValue(), timeCode, userKey);
-
-		int offset = hash[hash.length - 1] & 0xf;
-
-		int binary = ((hash[offset] & 0x7f) << 24) | ((hash[offset + 1] & 0xff) << 16) | ((hash[offset + 2] & 0xff) << 8)
-				| (hash[offset + 3] & 0xff);
-
-		int otp = (int) (binary % (Math.pow(10,numberOfDigits)));
-
-		result = Integer.toString(otp);
-		while (result.length() < numberOfDigits) {
-			result = "0" + result;
-		}
-		return result;
+		
+		return SHAs.TOTP(EXCEPTION_MSG, HashType.SHA1.getValue(), seconds, numberOfDigits, username, password);
 	}
-
-	private static byte[] sha1(String code, byte[] time, byte[] userKey) {
-		try {
-			Mac hashMsgCode;
-			hashMsgCode = Mac.getInstance(code);
-			SecretKeySpec secret = new SecretKeySpec(userKey, "RAW");
-			hashMsgCode.init(secret);
-			return hashMsgCode.doFinal(time);
-		} catch (GeneralSecurityException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Something went wrong while using sh1()");
-		}
-	}
-	
 }
 
